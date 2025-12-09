dc0333b9fbb54d68f73ed0cd46585365ec03c3ae
Supriyo Chowdhury <supriyochowdhury760@gmail.com>
2025-12-10 02:23:55 +0530

    Add free TURN servers to rtcConfig for better connectivity

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -59,9 +59,27 @@ const ALL_LANGUAGES = [
   { value: 'sql', label: 'SQL' },
 ];
 
-// STUN server for WebRTC (for demo / dev)
+// STUN/TURN servers for WebRTC
 const rtcConfig: RTCConfiguration = {
-  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:global.stun.twilio.com:3478' },
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+  ],
 };
 
 // ----------- Video Tile Component -------------
